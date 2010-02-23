e8fca3821c783094dee42bfabfcc90c3b628a4a2
skwak <skwak@34f4711e-aa42-3a49-b93c-796f75d5591e>
2010-02-23 13:04:04 +0000

    Musicam_2010_0223_2.zip libmusicam_dll 추가 및 musicamTest 프로젝트를 통해 검증

--- musicamTest/main.cpp
+++ musicamTest/main.cpp
@@ -1,17 +1,46 @@
 #include <iostream>
 #include <assert.h>
-#include <mp2Changer.h>
+#include <musicam.h>
 
 #define INPUT_FILE			"..//contents//streamList_subCh(7).mp2"
 #define OUTMP2_FILE			"..//output//new_streamList_subCh(7).mp2"
 #define MAX_SAMPLE_SIZE		(1152)
 #define MP2_BUF_SIZE		(16384)
 
+static const int bitrateTable[2][15]={
+	{0,8,16,24,32,40,48,56,64,80,96,112,128,144,160},		//for 24KHz
+	{0,32,48,56,64,80,96,112,128,160,192,224,256,320,384}	//for 48KHz
+};
+
+void parseMp2Header(MP2_HEADER* pHeader,unsigned char* pBuffer)
+{
+	pHeader->sync		= (((unsigned short)pBuffer[0]&0xFF)<<4) | (((unsigned short)pBuffer[1]&0xF0)>>4);
+	pHeader->id			= (pBuffer[1]&0x08)>>3;
+	pHeader->layer		= (pBuffer[1]&0x06)>>1;
+	pHeader->protect	= (pBuffer[1]&0x01);
+	pHeader->bitrateIdx	= (pBuffer[2]&0xF0)>>4;
+	pHeader->sampling	= (pBuffer[2]&0x0C)>>2;
+	pHeader->padding	= (pBuffer[2]&0x02)>>1;
+	pHeader->priv		= (pBuffer[2]&0x01);
+	pHeader->mode		= (pBuffer[3]&0xC0)>>6;
+	pHeader->mode_ext	= (pBuffer[3]&0x30)>>4;
+	pHeader->copyright	= (pBuffer[3]&0x08)>>3;
+	pHeader->orignal	= (pBuffer[3]&0x04)>>2;
+	pHeader->emphasis	= (pBuffer[3]&0x03);
+}
+
+static int getBitrate(int nId,int nBitrateIdx)
+{
+	assert(nId==0 || nId==1);
+	assert(nBitrateIdx>0 && nBitrateIdx<15);
+	return 1000*bitrateTable[nId][nBitrateIdx];	//kbps unit
+}
+
 using namespace std;
 
-void main(void)
+void main(int argc,int *argv[])
 {
-	mp2Changer* mp2c = mp2c_init();
+	mc* m = mc_init();
 
 	//Decode
 	FILE *inpStream,*mp2OutStream;
@@ -25,7 +54,7 @@ void main(void)
 		return;
 	}
 
-	twolame_options* encopts = mp2c->opt;
+	twolame_options* encopts = m->opt;
 
 	TWOLAME_MPEG_version ver = TWOLAME_MPEG1;
 	int nSrcNumCh = 2;
@@ -51,19 +80,20 @@ void main(void)
 	unsigned char buffer[MAX_SAMPLE_SIZE];
 	memset(buffer,0x0,sizeof(buffer));
 	unsigned char mp2Buffer[MP2_BUF_SIZE];
+	MP2_HEADER header;
 
 	int nFrameCount = 0;
 	while( !feof( inpStream ) ) {
 		size_t nRead = fread(buffer,sizeof(unsigned char),4,inpStream);	//read header only
-		mp2c_parseMp2Header(mp2c,buffer);
+		parseMp2Header(&header,buffer);
 
-		int nSrcBufferSize = 144*mp2c_bitrate(mp2c)/twolame_get_in_samplerate(encopts);
+		int nSrcBufferSize = 144*getBitrate(header.id,header.bitrateIdx)/((header.id)?48000:24000);
 		nRead = fread(buffer+4,sizeof(unsigned char),nSrcBufferSize-4,inpStream);	//read rest of the frame
 		if(nRead) assert(nRead==(nSrcBufferSize-4));
 
-		int nEncRtn = mp2c_encode(mp2c,buffer,MAX_SAMPLE_SIZE,mp2Buffer,sizeof(mp2Buffer));
+		int nEncRtn = mc_encode(m,buffer,nSrcBufferSize,MAX_SAMPLE_SIZE,mp2Buffer,sizeof(mp2Buffer));
 		if(nEncRtn<0) {
-			cout<<"encode error:"<<mp2c_getLastError()<<endl;
+			cout<<"encode error:"<<mc_getLastError()<<endl;
 			break;
 		}
 		size_t nWrite = fwrite(mp2Buffer,sizeof(unsigned char),nEncRtn,mp2OutStream);
@@ -73,9 +103,9 @@ void main(void)
 		fprintf(stderr, "]\r");
 		fflush(stderr);
 	}
-	mp2c_close(mp2c);
-
 	fclose(mp2OutStream);
 	fclose(inpStream);
+
+	mc_close(m);
 	cout<<"end of main"<<endl;
 };
