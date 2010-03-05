c832d4d740011d047b86128f93fa2dfb84c9064a
skwak <skwak@34f4711e-aa42-3a49-b93c-796f75d5591e>
2010-03-05 05:37:44 +0000

    Musicam_2010_0305_2.zip musicam.h 헤더에 api주석 추가 musicam에 api 추가 및 인터페이스 변경 musicamTest main.cpp변경

--- include/musicam.h
+++ include/musicam.h
@@ -1,3 +1,9 @@
+/*
+*	MUSICAM: a MUSICAM library for DAB
+*
+*	Copyright (C) 2010 Seung-Chul,Kwak
+*
+*/
 #ifndef __MUSICAM_H__
 #define __MUSICAM_H__
 
@@ -14,35 +20,113 @@ extern "C" {
 #endif
 
 #pragma pack(push, 1)
+	/** MPEG2 Audio frame header */
 	typedef struct{
-		unsigned short sync:12;
-		unsigned short id:1;
-		unsigned short layer:2;
-		unsigned short protect:1;
-		unsigned short bitrateIdx:4;
-		unsigned short sampling:2;
-		unsigned short padding:1;
-		unsigned short priv:1;
-		unsigned short mode:2;
-		unsigned short mode_ext:2;
-		unsigned short copyright:1;
-		unsigned short orignal:1;
-		unsigned short emphasis:2;
+		unsigned short sync:12;			/**< syncword */
+		unsigned short id:1;			/**< identifier */
+		unsigned short layer:2;			/**< layer */
+		unsigned short protect:1;		/**< protection bit */
+		unsigned short bitrateIdx:4;	/**< bit rate index */
+		unsigned short sampling:2;		/**< sampling frequency */
+		unsigned short padding:1;		/**< padding bit */
+		unsigned short priv:1;			/**< private bit */
+		unsigned short mode:2;			/**< mode */
+		unsigned short mode_ext:2;		/**< mode extension */
+		unsigned short copyright:1;		/**< copy right */
+		unsigned short orignal:1;		/**< original/copy */
+		unsigned short emphasis:2;		/**< emphasis */
 	}MP2_HEADER;
 #pragma pack(pop)
 
+	/** MUSICAM library context */
 	typedef struct _musicam_context {
-		MP2_HEADER*		 hdr;
-		mpg123_handle*	 mpg;
-		twolame_options* opt;
-		scfCrc*			 sc;
+		MP2_HEADER*		 hdr;			/**< MPEG2 Audio frame header */
+		mpg123_handle*	 mpg;			/**< pointer to mpg123 library */
+		twolame_options* opt;			/**< pointer to twolame option */
+		scfCrc*			 sc;			/**< pointer to scale factor crc library */
 	} mc;
 
+	/** ID of MPEG2 Audio frame header */
+	typedef enum {
+		ISO_13818_3 = 0,	/**< ISO/IEC 13818-3 */
+		ISO_11172_3 = 1		/**< ISO/IEC 11172-3 */
+	} HEADER_ID;
+
+	/** Initialize MUSICAM library.
+	*
+	*  \return					pointer to MUSICAM library
+	*/
 	MUSICAM_DLL_EXPORT mc* mc_init();
+
+	/** Uninitialize and close MUSICAM library
+	*
+	*	\param m				pointer to MUSICAM library
+	*	\return					void
+	*
+	*/
 	MUSICAM_DLL_EXPORT void mc_close(mc* m);
+
+	/** encode input pcm stream to mp2 stream
+	*	
+	*	\param m				pointer to musicam library
+	*	\param pcmFrame			pointer to pcm stream
+	*	\param pcmFrameSize		size of pcm stream
+	*	\param numSampleCh		number of samples per channel
+	*	\param outMucframe		pointer to encoded output buffer
+	*	\return					0 successful otherwise -1
+	*/
 	MUSICAM_DLL_EXPORT int mc_pcm_mp2_encode(mc* m,const unsigned char* pcmFrame,int pcmFrameSize,int numSampleCh,unsigned char* outMucframe);
+
+	/** transcode from given mp2 stream to another mp2 stream
+	*	
+	*	\param m				pointer to musicam library
+	*	\param inMp2frame		pointer to mp2 stream
+	*	\param inMp2frameSize	size of mp2 stream
+	*	\param numSampleCh		number of samples per channel
+	*	\param outMucframe		pointer to encoded output buffer
+	*	\return					0 successful otherwise -1
+	*/
 	MUSICAM_DLL_EXPORT int mc_mp2_mp2_encode(mc* m,const unsigned char* inMp2frame,int inMp2frameSize,int numSampleCh,unsigned char* outMucframe);
+
+	/** get last error message
+	*	
+	*	\return					pointer to error message buffer
+	*/
 	MUSICAM_DLL_EXPORT const char* mc_getLastError();
+
+	/** mp2 header parser
+	*	
+	*	\param pHeader			pointer to mp2 header
+	*	\param pBuffer			pointer to mp2 stream
+	*	\return					void
+	*/
+	MUSICAM_DLL_EXPORT void mc_parseMp2Header(MP2_HEADER* pHeader,unsigned char* pBuffer);
+
+	/** get sampling frequency from given identifier
+	*	
+	*	\param id				identifier in mp2 header
+	*	\return					sampling frequency in Hz, -1 if not successful
+	*/
+	MUSICAM_DLL_EXPORT int mc_getSamplingFrequency(int id);
+
+	/** get bitrate
+	*	
+	*	\param nId				identifier in mp2 header
+	*	\param nBitrateIdx		bitrate index (1~14)
+	*	\return					bitrate in bps, -1 if not successful
+	*/
+	MUSICAM_DLL_EXPORT int mc_getBitrate(int nId,int nBitrateIdx);
+
+	/** set mp2 encode option
+	*	
+	*	\param encopts			pointer to twolame encode option
+	*	\param id				identifier in mp2 header (ISO_13818_3,ISO_11172_3)
+	*	\param nDstBitrate		desired bitrate of encoded birstream (kbps)
+	*	\param mpegMode			mode (streo,joint stereo,dual channel,mono)
+	*	\param verbose			verbosity control
+	*	\return					0 successful otherwise -1
+	*/
+	MUSICAM_DLL_EXPORT int mc_encodeOption(twolame_options* encopts,HEADER_ID id,int nDstBitrate,TWOLAME_MPEG_mode mpegMode,int verbose);
 
 #ifdef __cplusplus
 }
--- musicamTest/main.cpp
+++ musicamTest/main.cpp
@@ -5,74 +5,18 @@
 #define MAX_SAMPLE_SIZE		(1152)
 #define MP2_BUF_SIZE		(16384)
 
-static const int bitrateTable[2][15]={
-	{0,8,16,24,32,40,48,56,64,80,96,112,128,144,160},		//for 24KHz
-	{0,32,48,56,64,80,96,112,128,160,192,224,256,320,384}	//for 48KHz
-};
-
 using namespace std;
-
-void parseMp2Header(MP2_HEADER* pHeader,unsigned char* pBuffer)
-{
-	pHeader->sync		= (((unsigned short)pBuffer[0]&0xFF)<<4) | (((unsigned short)pBuffer[1]&0xF0)>>4);
-	pHeader->id			= (pBuffer[1]&0x08)>>3;
-	pHeader->layer		= (pBuffer[1]&0x06)>>1;
-	pHeader->protect	= (pBuffer[1]&0x01);
-	pHeader->bitrateIdx	= (pBuffer[2]&0xF0)>>4;
-	pHeader->sampling	= (pBuffer[2]&0x0C)>>2;
-	pHeader->padding	= (pBuffer[2]&0x02)>>1;
-	pHeader->priv		= (pBuffer[2]&0x01);
-	pHeader->mode		= (pBuffer[3]&0xC0)>>6;
-	pHeader->mode_ext	= (pBuffer[3]&0x30)>>4;
-	pHeader->copyright	= (pBuffer[3]&0x08)>>3;
-	pHeader->orignal	= (pBuffer[3]&0x04)>>2;
-	pHeader->emphasis	= (pBuffer[3]&0x03);
-}
-
-static int getBitrate(int nId,int nBitrateIdx)
-{
-	assert(nId==0 || nId==1);
-	assert(nBitrateIdx>0 && nBitrateIdx<15);
-	return 1000*bitrateTable[nId][nBitrateIdx];	//kbps unit
-}
-
-int getSamplingFrequency(int id)
-{
-	assert(id==0||id==1);
-	if(id==1) { return 48000; }else{ return 24000; }
-}
-
-int initTwolameOption(twolame_options* encopts,MP2_HEADER* header,int nDstBitrate)
-{
-	twolame_set_version(encopts,(header->id==1)?TWOLAME_MPEG1:TWOLAME_MPEG2);
-	twolame_set_rawmode(encopts, 1);
-	twolame_set_num_channels(encopts,(header->mode==0x3)?1:2);
-	twolame_set_in_samplerate(encopts,getSamplingFrequency(header->id));
-	twolame_set_out_samplerate(encopts,getSamplingFrequency(header->id));
-	twolame_set_mode(encopts,TWOLAME_STEREO);
-	twolame_set_bitrate(encopts,nDstBitrate);
-	twolame_set_error_protection(encopts,TRUE);
-
-	if(twolame_init_params(encopts)!=0) {
-		cout<<"Error: configuring libtwolame encoder failed"<<endl;
-		return -1;
-	}
-
-	cout<<"Input file bitrate: "<<getBitrate(header->id,header->bitrateIdx)/1000<<"kbps"<<endl;
-	twolame_print_config(encopts);
-	return 0;
-}
 
 void main(int argc, char *argv[])
 {
-	if (argc!=4) {
-		cout<<"Usage: musicamTest <input.mp2> <output.mp2> <output bitrate>"<<endl;
+	if (argc<6) {
+		cout<<"Usage: musicamTest <input.mp2> <output.mp2> <output id> <output bitrate> <output mode>"<<endl;
 		return;
 	}
 
 	cout<<"Input file: "<<argv[1]<<endl;
 	cout<<"Output file: "<<argv[2]<<endl;
-	cout<<"Desired Output Bitrate:"<<argv[3]<<"kbps"<<endl;
+	cout<<"Desired Output Bitrate:"<<argv[4]<<"kbps"<<endl;
 
 	mc* m = mc_init();
 
@@ -96,16 +40,16 @@ void main(int argc, char *argv[])
 	int nFrameCount = 0;
 	while( !feof( inpStream ) ) {
 		size_t nRead = fread(buffer,sizeof(unsigned char),4,inpStream);	//read header only
-		parseMp2Header(&header,buffer);
+		mc_parseMp2Header(&header,buffer);
 
 		if(nFrameCount==0) {
-			if(initTwolameOption(m->opt,&header,atoi(argv[3]))!=0) {
-				cout<<"initTwolameOption() failed"<<endl;
+			if(mc_encodeOption(m->opt,(HEADER_ID)atoi(argv[3]),atoi(argv[4]),(TWOLAME_MPEG_mode)atoi(argv[5]),1)!=0) {
+				cout<<mc_getLastError();
 				break;
 			}
 		}
 
-		int nSrcBufferSize = 144*getBitrate(header.id,header.bitrateIdx)/getSamplingFrequency(header.id);
+		int nSrcBufferSize = 144*mc_getBitrate(header.id,header.bitrateIdx)/mc_getSamplingFrequency(header.id);
 		nRead = fread(buffer+4,sizeof(unsigned char),nSrcBufferSize-4,inpStream);	//read rest of the frame
 		if(nRead) assert(nRead==(nSrcBufferSize-4));
 
