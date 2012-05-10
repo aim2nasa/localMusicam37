5c0cd8e0e2b1ab253c8c40611ee1af7ef61067ea
skwak <skwak@34f4711e-aa42-3a49-b93c-796f75d5591e>
2012-05-10 07:45:10 +0000

    get_bits함수를 rBit로 모듈화하여 이를 적용함

---
+++ mp2musicamTest/rbit.h
@@ -0,0 +1,31 @@
+/******************************************************************************
+** RBIT -- Bitwise Read Utility												 **
+** cf. window size is set as 2 bytes										 **
+******************************************************************************/
+#ifndef __RBIT_H__
+#define __RBIT_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef DLL_EXPORT
+#define DLL_EXPORT __declspec(dllexport)
+#endif
+
+	typedef struct _rBit_context {
+		int bit_window;
+		int bits_in_window;
+		const unsigned char *frame_pos;
+	} rBit;
+
+	DLL_EXPORT rBit* rBit_instance();
+	DLL_EXPORT void rBit_delete(rBit* p);
+	DLL_EXPORT void rBit_init(rBit* p,unsigned char *pFrame);
+	DLL_EXPORT int rBit_getBits(rBit* p,int bit_count);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif//__RBIT_H__
